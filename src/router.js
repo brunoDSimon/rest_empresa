173fded1b535d0c19baa3630f0f27dd824b981a0
bruno <brunosimon999@gmail.com>
2020-03-31 10:19:29 -0300

    tentativa

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 const cors = require('cors');
 const app = express();
 
-app.use((req,res, next) =>{
+routes.use((req,res, next) =>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type', 'Accept', 'Authorization');
     if(res.method == 'OPTIONS'){
