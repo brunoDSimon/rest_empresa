19a54d8014c23e14fb5f7b6e36ab228bde9d063f
bruno <brunosimon999@gmail.com>
2020-04-21 19:40:31 -0300

    bcrypt

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,6 +2,8 @@ const Bead = require("../models/Bead");
 const Companies = require("../models/Companies");
 const Sequelize = require('sequelize');
 const Users = require('../models/Users');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
     async index(req,res){
         const users = await Users.findAll({
@@ -11,8 +13,9 @@ module.exports = {
     },
     async store(req,res){
         const {email, password, name} = req.body;
-
-        const users = await Users.create({email, password, name});
+        var salt = await bcrypt.genSalt(10);
+        var hash = await bcrypt.hashSync(password, salt)
+        const users = await Users.create({email: email, password: hash, name: name});
         return res.json(users);
     }
 }
