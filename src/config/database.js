0ed52388aadbe78c6bd0e029181c2bbfad60ea70
Bruno <brunosimon999@gmail.com>
2020-07-19 16:28:33 -0300

    mysql heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,13 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'root',
-    database: 'empresarest2',
+    host: 'us-cdbr-east-02.cleardb.com',
+    username: 'be18022bd486fd',
+    password: '08fbf2c3',
+    database: 'heroku_89e921c4f3234e5',
     define:{
         timestamps: true,
         underscored: false,
         timezone: "-03:00"
     },
 };
+// be18022bd486fd:08fbf2c3@us-cdbr-east-02.cleardb.com/heroku_89e921c4f3234e5
