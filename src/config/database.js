08dd308d994268c42c9d255d1d59684739953a59
Bruno <brunosimon999@gmail.com>
2020-07-20 18:47:13 -0300

    banco heroku

--- src/config/database.js
+++ src/config/database.js
@@ -12,5 +12,5 @@ module.exports = {
 };
 // bd7578395d7a7e:df201cb3@us-cdbr-east-02.cleardb.com/heroku_89b16f0f06faa82?reconnect=true
 
-mysql://bd7578395d7a7e:df201cb3@us-cdbr-east-02.cleardb.com/heroku_89b16f0f06faa82?reconnect=true
+// mysql://bd7578395d7a7e:df201cb3@us-cdbr-east-02.cleardb.com/heroku_89b16f0f06faa82?reconnect=true
 // heroku config:set DATABASE_URL='mysql://bd7578395d7a7e:df201cb3@us-cdbr-east-02.cleardb.com/heroku_89b16f0f06faa82?reconnect=true'
